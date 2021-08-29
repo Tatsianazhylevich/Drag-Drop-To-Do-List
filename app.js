379c2dd8de035faed86a243344caef5ff84de8f0
tatsiana zhylevich <tatzhylevich@gmail.com>
2021-08-29 15:03:57 +0300

    add LS for todolist

--- app.js
+++ app.js
@@ -3,22 +3,47 @@ const placeholders = document.querySelectorAll('.placeholder');
 const buttonAddTask = document.querySelector(".add_button");
 const buttonDeleteAll = document.querySelector(".del_button");
 
-const startedList = document.querySelector('.started');
-const inProgressList = document.querySelector('.inProgress');
-const finishList = document.querySelector('.finish');
+ const startedList = document.querySelector('.started');
+// const inProgressList = document.querySelector('.inProgress');
+// const finishList = document.querySelector('.finish');
+let draggedItem;
 
-console.log(startedList)
+const todoKey = 'todo';
+const inProgressKey = 'inProgress';
+const finishKey = 'finish';
+
+// Проверка хранилища
+const todoFromLS = localStorage.getItem(todoKey) ? JSON.parse(localStorage.getItem(todoKey)) : [];
+const inProgressFromLS = localStorage.getItem(inProgressKey) ? JSON.parse(localStorage.getItem(inProgressKey)) : [];
+const finishFromLS = localStorage.getItem(finishKey) ? JSON.parse(localStorage.getItem(finishKey)) : [];
+
+function addTaskToLocalStorage(key, taskList) {
+    localStorage.setItem(`${key}`, JSON.stringify(taskList));
+}
+
+for (let item of todoFromLS) {
+    createNewTask(item);
+}
+for (let item of inProgressFromLS) {
+    createNewTask(item);
+}
+for (let item of finishFromLS) {
+    createNewTask(item);
+}
 
 buttonAddTask.addEventListener('click', () => {
     if (inputTask.value !== "") {
+        todoFromLS.push(inputTask.value);
+        addTaskToLocalStorage(todoKey, todoFromLS);
         createNewTask(inputTask.value);
         inputRemove();
     }
-
 })
 
 buttonDeleteAll.addEventListener('click', () => {
     removeAllTasks();
+    localStorage.clear();
+  
 })
 
 function createNewTask(text) {
@@ -34,58 +59,57 @@ function createNewTask(text) {
     removeBtn.append(document.createTextNode('X'));
     task.append(removeBtn);
 
-
-  
     task.addEventListener('dragstart', dragstart);
     task.addEventListener('dragend', dragend);
 
-    for (const placeholder of placeholders) {
-        placeholder.addEventListener('dragover', dragover)
-        placeholder.addEventListener('dragenter', dragenter)
-        placeholder.addEventListener('dragleave', dragleave)
-        placeholder.addEventListener('drop', dragdrop)
-    }
-    
-    function dragstart (event) {
-        draggedItem = event.target;
-        event.target.classList.add('hold');
-        setTimeout(() => event.target.classList.add('hide'), 0)
-    }
-    
-    function dragend (event) {
-        event.target.className = 'item';
-    }
-    
-    function dragover (event) {
-        event.preventDefault();
-    }
-    
-    function dragenter (event) {
-        event.target.classList.add('hovered');
-    }
-    
-    function dragleave (event) {
-        event.target.classList.remove('hovered');
-    }
-    
-    function dragdrop (event) {
-        event.target.classList.remove('hovered');
-        
-        event.target.append(draggedItem);
-    }
-
     removeBtn.addEventListener('click', removeTask);
+
     function removeTask() {
         task.remove();
     }
 }
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', dragdrop);
+}
+
+function dragstart (event) {
+    draggedItem = event.target;
+    event.target.classList.add('hold');
+    setTimeout(() => event.target.classList.add('hide'), 0)
+}
+
+function dragend (event) {
+    event.target.className = 'item';
+}
+
+function dragover (event) {
+    event.preventDefault();
+}
+
+function dragenter (event) {
+    event.target.classList.add('hovered');
+}
+
+function dragleave (event) {
+    event.target.classList.remove('hovered');
+}
+
+function dragdrop (event) {
+    event.target.classList.remove('hovered');
+    event.target.append(draggedItem);
+}
+
 
 function inputRemove() {
     inputTask.value = "";
 }
 
 function removeAllTasks () {
-    placeholders.forEach(placeholder => placeholder.innerHTML = "")
+    placeholders.forEach(placeholder => placeholder.innerHTML = "");
 }
 
 
